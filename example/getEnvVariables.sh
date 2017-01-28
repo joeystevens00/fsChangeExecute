f8b3cafae8bf26085124ce1b214f2cc7d663c931
jstevens00 <joeystevens00@gmail.com>
2017-01-28 16:47:49 -0500

    getEnvVariables.sh

---
+++ example/getEnvVariables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "ENV_isDirectory=$ENV_isDirectory
+ENV_eventFile=$ENV_eventFile
+ENV_monitoredDirectory=$ENV_monitoredDirectory
+ENV_eventDirectory=$ENV_eventDirectory
+ENV_executedFromDirectory=$ENV_executedFromDirectory
+ENV_eventType=$ENV_eventType"
